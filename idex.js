7eb0099da825bb23de18f5655592fb2d910bb419
Gustavo2807 <g.rezendeberteli@gmail.com>
2023-12-28 20:35:55 -0300

    projeto finalizado

--- idex.js
+++ idex.js
@@ -1,1 +1,33 @@
-console.log("eai sistema")
+let nomeDoHeroi = "Gustavo"
+let XP = 1000
+
+
+switch (XP) {
+  case XP < 1000:
+    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE FERRO!");
+    break;
+  case XP >= 1001:
+    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE BRONZE");
+    break;
+  case XP >= 2001:
+    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE PRATA");
+    break;
+  case XP >= 5001:
+    console.log(nomeDoHeroi  + "ESTÁ NO NIVEL DE OURO!");
+    break;
+  case XP >= 7001:
+    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE PLATINA!");
+    break;
+ case XP >= 8001:
+    console.log(nomeDoHeroi + "ESTÁ NO NIVEL ACENDENTE!")
+    break;
+ case XP >= 9001:
+    console.log(nomeDoHeroi + "ESTÁ NO NIVEL IMORTAL!")
+    break;
+case XP >= 100001:
+    console.log(nomeDoHeroi + "ESTÁ NO NIVEL RADIANTE!")
+
+
+}
+
+
