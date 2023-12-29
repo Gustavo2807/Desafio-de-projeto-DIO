269d33c844307aed62b38f217afa2d8f121ccef1
Gustavo2807 <g.rezendeberteli@gmail.com>
2023-12-28 21:37:24 -0300

    projeto

--- idex.js
+++ idex.js
@@ -1,33 +1,20 @@
-let nomeDoHeroi = "Gustavo"
-let XP = 100
+let nomeDoHeroi = "guilherme1!@44";
+let XP = 100;
 
-
-switch (XP) {
-  case XP < 1000:
-    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE FERRO!");
-    break;
-  case XP >= 1001:
-    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE BRONZE");
-    break;
-  case XP >= 2001:
-    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE PRATA");
-    break;
-  case XP >= 5001:
-    console.log(nomeDoHeroi  + "ESTÁ NO NIVEL DE OURO!");
-    break;
-  case XP >= 7001:
-    console.log(nomeDoHeroi  +  "ESTÁ NO NIVEL DE PLATINA!");
-    break;
- case XP >= 8001:
-    console.log(nomeDoHeroi + "ESTÁ NO NIVEL ACENDENTE!")
-    break;
- case XP >= 9001:
-    console.log(nomeDoHeroi + "ESTÁ NO NIVEL IMORTAL!")
-    break;
-case XP >= 100001:
-    console.log(nomeDoHeroi + "ESTÁ NO NIVEL RADIANTE!")
-
-
+if (XP < 1000) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL DE FERRO!");
+} else if (XP >= 1001 && XP < 2001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL DE BRONZE");
+} else if (XP >= 2001 && XP < 5001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL DE PRATA");
+} else if (XP >= 5001 && XP < 7001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL DE OURO!");
+} else if (XP >= 7001 && XP < 8001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL DE PLATINA!");
+} else if (XP >= 8001 && XP < 9001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL ACENDENTE!");
+} else if (XP >= 9001 && XP < 100001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL IMORTAL!");
+} else if (XP >= 100001) {
+  console.log(nomeDoHeroi + " ESTÁ NO NÍVEL RADIANTE!");
 }
-
-
